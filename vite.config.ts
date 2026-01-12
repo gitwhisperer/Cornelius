6b184f777deb59c34788f52ab941c6dee59460df
GitWhisperer <harshittinker1979@gmail.com>
2026-01-12 23:21:56 +0530

    added   base: '/Cornelius/' in vite,config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: '/Cornelius/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
